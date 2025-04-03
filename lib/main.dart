b355a7d73f8f2868c5659f0b1cb248bb8a8205df
rahull0328 <rmmehta2004@gmail.com>
2025-04-03 22:28:37 +0530

    stuck at integrating the api

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:bytenews/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
